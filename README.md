8acbfa1cb3fc0edde12ec17cbb278470a8e2ebc7
Juan <juan.roa-h@mail.escuelaing.edu.co>
2026-06-04 10:59:41 -0500

    feat: exercise 2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ![ex1](images\ex1.png)
+![ex2](images\ex2.png)
--- src/Ex2/URLReader.java
+++ src/Ex2/URLReader.java
@@ -1,4 +1,36 @@
 package Ex2;
 
+import java.io.IOException;
+import java.net.*;
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+
 public class URLReader {
+    public static void main(String[] args) {
+        URL url = readUrl();
+        saveUrlInFile(url);
+    }
+
+    private static void saveUrlInFile(URL url) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))){
+            FileWriter fw = new FileWriter("html/resultado.html");
+            String inputLine;
+            while ((inputLine = reader.readLine()) != null){
+                fw.write(inputLine);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static URL readUrl() {
+        String url = System.console().readLine("Digite una URL: ");
+        try {
+            return new URL(url.trim());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
